868660103fda0653c4065f6147e9f508462fa1a5
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-01 21:14:19 +0100

    Create libro isbn exists test green

--- src/main/java/com/kairosds/cursospb2/biblioteca/libroisbn/service/LibroISbnServiceImpl.java
+++ src/main/java/com/kairosds/cursospb2/biblioteca/libroisbn/service/LibroISbnServiceImpl.java
@@ -2,13 +2,14 @@ package com.kairosds.cursospb2.biblioteca.libroisbn.service;
 
 import com.kairosds.cursospb2.biblioteca.config.CreditsConfiguration;
 import com.kairosds.cursospb2.biblioteca.libroisbn.domain.LibroISBN;
+import com.kairosds.cursospb2.biblioteca.libroisbn.domain.exception.CreateLibroISBNExists;
 import com.kairosds.cursospb2.biblioteca.libroisbn.repository.LibroIsbnRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
-public class LibroISbnServiceImpl implements LibroIsbnService{
+public class LibroISbnServiceImpl implements LibroIsbnService {
 
     private final LibroIsbnRepository libroIsbnRepository;
 
@@ -16,6 +17,13 @@ public class LibroISbnServiceImpl implements LibroIsbnService{
 
     @Override
     public LibroISBN createLibroIsbn(LibroISBN libroIsbn) {
+
+        final var isbn = libroIsbn.getIsbn();
+        final var isbnExists = this.libroIsbnRepository.existsByIsbn(isbn);
+
+        if (isbnExists) {
+            throw new CreateLibroISBNExists(isbn);
+        }
         return null;
     }
 }
