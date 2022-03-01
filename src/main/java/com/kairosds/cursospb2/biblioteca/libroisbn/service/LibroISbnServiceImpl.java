454180254fc44fdd3ee9fc4ce087433245ff3b6e
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-01 21:12:53 +0100

    Test in yellow

--- src/main/java/com/kairosds/cursospb2/biblioteca/libroisbn/service/LibroISbnServiceImpl.java
+++ src/main/java/com/kairosds/cursospb2/biblioteca/libroisbn/service/LibroISbnServiceImpl.java
@@ -1,10 +1,19 @@
 package com.kairosds.cursospb2.biblioteca.libroisbn.service;
 
+import com.kairosds.cursospb2.biblioteca.config.CreditsConfiguration;
 import com.kairosds.cursospb2.biblioteca.libroisbn.domain.LibroISBN;
+import com.kairosds.cursospb2.biblioteca.libroisbn.repository.LibroIsbnRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class LibroISbnServiceImpl implements LibroIsbnService{
+
+    private final LibroIsbnRepository libroIsbnRepository;
+
+    private final CreditsConfiguration creditsConfiguration;
+
     @Override
     public LibroISBN createLibroIsbn(LibroISBN libroIsbn) {
         return null;
