5eb087cdfe4ac422c4052e5de9e0eab2ea935771
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-01 22:10:18 +0100

    fix application context - libro service impl created

---
+++ src/main/java/com/kairosds/cursospb2/biblioteca/libro/service/LibroServiceImpl.java
@@ -0,0 +1,13 @@
+package com.kairosds.cursospb2.biblioteca.libro.service;
+
+import com.kairosds.cursospb2.biblioteca.libro.domain.Libro;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibroServiceImpl implements LibroService{
+    
+    @Override
+    public Libro createLibro(Libro libro) {
+        return null;
+    }
+}
