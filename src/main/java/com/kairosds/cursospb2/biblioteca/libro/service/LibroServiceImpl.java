a7c4f59f75286e87c698a5b6531b4a8c765e7aa0
Sergio Gumiel <sergio.gumiel@kairosds.com>
2022-03-02 07:52:28 +0100

    Libro service all test green

--- src/main/java/com/kairosds/cursospb2/biblioteca/libro/service/LibroServiceImpl.java
+++ src/main/java/com/kairosds/cursospb2/biblioteca/libro/service/LibroServiceImpl.java
@@ -1,6 +1,8 @@
 package com.kairosds.cursospb2.biblioteca.libro.service;
 
 import com.kairosds.cursospb2.biblioteca.libro.domain.Libro;
+import com.kairosds.cursospb2.biblioteca.libro.domain.exception.CreateLibroCodeExists;
+import com.kairosds.cursospb2.biblioteca.libro.domain.exception.CreateLibroIsbnNotExists;
 import com.kairosds.cursospb2.biblioteca.libro.repository.LibroRepository;
 import com.kairosds.cursospb2.biblioteca.libroisbn.repository.LibroIsbnRepository;
 import lombok.AllArgsConstructor;
@@ -10,12 +12,28 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class LibroServiceImpl implements LibroService{
 
-    private final LibroIsbnRepository libroIsbnRepository;
-
     private final LibroRepository libroRepository;
+
+    private final LibroIsbnRepository libroIsbnRepository;
 
     @Override
     public Libro createLibro(Libro libro) {
-        return Libro.builder().build();
+
+        final var isbn = libro.getIsbn();
+        final var isbnExists = this.libroIsbnRepository.existsByIsbn(isbn);
+
+        if(!isbnExists) {
+            throw new CreateLibroIsbnNotExists(isbn);
+        }
+
+        final var codigo = libro.getCodigo();
+        final var codigoExists = this.libroRepository.existsByCodigo(codigo);
+
+        if(codigoExists) {
+            throw new CreateLibroCodeExists(codigo);
+        }
+
+        final var libroSaved = this.libroRepository.save(libro);
+        return libroSaved;
     }
 }
